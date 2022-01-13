0128b8d9c44c03ddf940276fe8e241cc0ddfc599
abhi <assomvanshi7@gmail.com>
2022-01-13 20:51:26 +0530

    post card jsx layout done

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ posts }) {
 
       <div className='grid gird-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post, index) => <PostCard post={post.node} key={post.title} />)}
         </div>
 
         <div className='lg:col-span-4 col-span-1'>
