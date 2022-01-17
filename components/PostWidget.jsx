219b6f38ad6d5294629d3c19cb99ede8b8c7d396
abhi <assomvanshi7@gmail.com>
2022-01-17 23:05:19 +0530

    changes in color still needs further changes

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -22,7 +22,7 @@ const PostWidget = ({ categories, slug }) => {
   }, [slug]);
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+    <div className="bg-cyan-800 shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
       {relatedPosts.map((post, index) => (
         <div key={index} className="flex items-center w-full mb-4">
