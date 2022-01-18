189a4a889984769007c3767b06f7e3a6e21c0144
abhi <assomvanshi7@gmail.com>
2022-01-18 23:08:32 +0530

    Author.jsx done

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const PostCard = ({post}) => {
     console.log(post);
     return (
-        <div className='bg-cyan-300 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+        <div className='bg-neutral-900 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
             <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
                 <img  
                     src={post.featuredImage.url}
