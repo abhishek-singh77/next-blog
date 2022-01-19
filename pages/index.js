8352d96d2db1e8ed6860de7969b16521d689f30d
abhi <assomvanshi7@gmail.com>
2022-01-19 22:27:02 +0530

    backend done

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,32 @@
-import Head from 'next/head'
-import {PostCard, PostWidget, Categories} from '../components';
-import {getPosts} from '../services'
-
-
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
 export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8 ">
-      <Head>
-        <title>LetsBlog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div className='grid gird-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => <PostCard post={post.node} key={post.title} />)}
+    <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
-
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
+// Fetch data at build time
+export async function getStaticProps() {
   const posts = (await getPosts()) || [];
   return {
-    props: {posts}
-  }
+    props: { posts },
+  };
 }
